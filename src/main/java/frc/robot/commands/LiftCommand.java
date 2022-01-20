268cd16ffa5536cb17f8bbe237e6cded0e498ff4
Anirudh Chari <anirudh.chari05@gmail.com>
2022-01-19 18:40:53 -0600

    added dstar test files

---
+++ src/main/java/frc/robot/commands/LiftCommand.java
@@ -0,0 +1,35 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.LiftSubsystem;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.constants;
+
+public class LiftCommand extends CommandBase {
+    
+    private static LiftSubsystem subsystem;
+
+    public LiftCommand(LiftSubsystem subsystem) {
+        this.subsystem = subsystem;
+        addRequirements(subsystem);
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        subsystem.moveByVelocity(CONTROLLER.getY(hand.kRight));
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        subsystem.moveByVelocity(0);
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+}
+
--- src/main/java/frc/robot/subsystems/LiftSubsystem.java
+++ src/main/java/frc/robot/subsystems/LiftSubsystem.java
@@ -4,33 +4,62 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LiftSubsystem extends SubsystemBase {
 
-    private static double MAX_POSITION = 0;
-    private static double MIN_POSITION = 0;
-    private static double MAX_VELOCITY = 0;
+    private static final double MAX_POSITION = 0;
+    private static final double MIN_POSITION = 0;
+    private static final double MAX_VELOCITY = 0;
+    private static final double MPS_TO_TPHMS = 0;
 
-    private final WPI_TalonFX leftMotor;
-    private final WPI_TalonFX rightMotor;
-    private final Encoder leftEncoder;
-    private final Encoder rightEncoder;
-    private double position;
+    private static final WPI_TalonFX leftMotor = new WPI_TalonFX(0);
+    private static final WPI_TalonFX rightMotor = new WPI_TalonFX(1);
+    private static final Encoder leftEncoder = new Encoder(4, 5);
+    private static final Encoder rightEncoder = new Encoder(6, 7);
+    private static final DigitalInput topLimitSwitch = new DigitalInput(8);
+    private static final DigitalInput bottomLimitSwitch = new DigitalInput(8);
     
     public LiftSubsystem() {
-        leftMotor = new WPI_TalonFX(0);
-        rightMotor = new WPI_TalonFX(1);
-        rightMotor.set();
-        leftEncoder = new Encoder(4, 5);
-        rightEncoder = new Encoder(6, 7);
-        position = 0;
+        
+        rightMotor.setInverted();
+        rightMotor.follow(leftMotor);
+        rightMotor.setSensorPhase(false);
+        leftMotor.setSensorPhase(false);
+
+        leftMotor.configRemoteFeedbackSensor(leftEncoder, 0);
+        rightMotor.configRemoteFeedbackSensor(rightEncoder, 0);
+        leftMotor.configSelectedFeedbackSensor(TalonFXFeedbackDevice.RemoteSensor0);
+        rightMotor.configSelectedFeedbackSensor(TalonFXFeedbackDevice.RemoteSensor0);
+
+        leftEncoder.configSensorInitializationStrategy(SensorInitializationStrategy.BootToAbsolutePosition);
+        rightEncoder.configSensorInitializationStrategy(SensorInitializationStrategy.BootToAbsolutePosition);
+
+        leftEncoder.configMagnetOffset(0);
+        rightEncoder.configMagnetOffset(0);
+    
+    }
+    
+    /**
+     * Moves lift based on a given position value.
+     * @param position Position value to move to
+     */
+    public void moveByPosition(double position) {
+        leftMotor.set(ControlMode.Position, Math.max(Math.min(position, MAX_POSITION), MIN_POSITION));
+    }
+    
+    /**
+     * Moves lift based on a given velocity value.
+     * @param velocity Velocity value to follow for move.
+     */
+    public void moveByVelocity(double velocity) {
+        leftMotor.set(ControlMode.Velocity, Math.min(velocity, MAX_VELOCITY) * MPS_TO_TPHMS);
     }
 
-    public void move(double position, double velocity) {
-        this.position = Math.max(Math.min(position, MAX_POSITION), MIN_POSITION);
-        this.velocity = Math.min(velocity, MAX_VELOCITY);
-        rightMotor // counterclockwise
-    }
-
-    public double getPosition() {
-        return position;
+    /**
+     * Periodically checks whether limit switches are activated.
+     */
+    @Override
+    public void periodic() {
+        if (topLimitSwitch.get() || bottomLimitSwitch.get()) {
+            leftMotor.set(ControlMode.PercentOutput, 0);
+        }
     }
 
 }
