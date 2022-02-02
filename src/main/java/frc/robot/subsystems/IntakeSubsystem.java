f5e54e725494d564b22d583027d844df8362776e
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2022-02-01 19:04:11 -0600

    Motor Config

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -2,6 +2,9 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
+import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
+import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -12,9 +15,17 @@ public class IntakeSubsystem extends SubsystemBase {
     private static final int MOTOR_PORT = 0;
 
     private static final WPI_TalonFX motor = new WPI_TalonFX(MOTOR_PORT);
+    private static final SupplyCurrentLimitConfiguration MAX_AMPS = new SupplyCurrentLimitConfiguration(true, 10, 0, 0);
+    private static final StatorCurrentLimitConfiguration MAX_AMPS_OUT = new StatorCurrentLimitConfiguration(true, 10, 0, 0);
 
     public IntakeSubsystem() {
         motor.setNeutralMode(NeutralMode.Brake);
+        motor.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, 0);
+        motor.setInverted(false);
+        motor.configSupplyCurrentLimit(MAX_AMPS);
+        motor.configStatorCurrentLimit(MAX_AMPS_OUT);
+        motor.configNeutralDeadband(0.01);
+        motor.selectProfileSlot(0, 0);
     }
 
     public static IntakeSubsystem test() {
