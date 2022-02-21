af7ad0db322fdf8034fa47b52fe40965301cabc2
cplepel-py <cbplepel@gmail.com>
2022-02-20 19:46:32 -0600

    Replaced ManualSwerveDriveCommand with HolonomicDriveCommand

--- src/main/java/frc/robot/config/SwerveDriveContainer.java
+++ src/main/java/frc/robot/config/SwerveDriveContainer.java
@@ -2,12 +2,13 @@ package frc.robot.config;
 
 import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
 
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import frc.robot.commands.ManualSwerveDriveCommand;
-import frc.robot.config.PIDConfig;
+import frc.robot.commands.HolonomicDriveCommand;
 //import frc.robot.subsystems.NavigationSubsystem;
 import frc.robot.subsystems.SwerveDriveSubsystem;
+import frc.robot.config.RobotContainer;
 
 /**
  * This class is where the bulk of the robot should be declared. Since
@@ -27,6 +28,8 @@ public class SwerveDriveContainer implements RobotContainer {
     private final ParallelCommandGroup autoGroup;
     private final ParallelCommandGroup teleopGroup;
 
+    private static final XboxController xbox = new XboxController(1);
+
     /**
      * The container for the robot. Contains subsystems, OI devices, and commands.
      */
@@ -40,7 +43,7 @@ public class SwerveDriveContainer implements RobotContainer {
         // Initialize Auto Commands
 
         // Initialize Teleop Commands
-        ManualSwerveDriveCommand manualDriveCommand = new ManualSwerveDriveCommand(swerveDriveSub/*, navSub*/, getSwerveHeadingPIDConfig());
+        HolonomicDriveCommand manualDriveCommand = new HolonomicDriveCommand(swerveDriveSub, xbox);
         // ManualVHopperCommand vhopperCommand = new ManualVHopperCommand(vhopperSub);
         // ManualWristCommand intakeCommand = new ManualWristCommand(intakeSub);
 
