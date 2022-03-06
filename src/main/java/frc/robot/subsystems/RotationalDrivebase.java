df5e456a78c6decd6ab39f17e6bf1ae4aa304070
cplepel-py <cbplepel@gmail.com>
2022-03-06 17:49:13 -0600

    Added rotational drive command

--- src/main/java/frc/robot/subsystems/RotationalDrivebase.java
+++ src/main/java/frc/robot/subsystems/RotationalDrivebase.java
@@ -1,9 +1,11 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj2.command.Subsystem;
+
 /**
  * A drivebase capable of rotation.
  */
-public interface RotationalDrivebase {
+public interface RotationalDrivebase extends Subsystem {
     /**
      * Sets the rotational velocity in radians per second.
      * 
