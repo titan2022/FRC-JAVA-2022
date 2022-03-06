2954c422107f39caf8c5a3f4302f0e1f77c069a7
cplepel-py <cbplepel@gmail.com>
2022-03-06 16:55:54 -0600

    Added translational and rotational drivebase interfaces

---
+++ src/main/java/frc/robot/subsystems/RotationalDrivebase.java
@@ -0,0 +1,19 @@
+package frc.robot.subsystems;
+
+/**
+ * A drivebase capable of rotation.
+ */
+public interface RotationalDrivebase {
+    /**
+     * Sets the rotational velocity in radians per second.
+     * 
+     * @param omega  The desired rotational velocity in radians per second.
+     */
+    public void setRotation(double omega);
+    /**
+     * Returns the current rotational velocity in radians per second.
+     * 
+     * @return  The current rotational velocity in radians per second.
+     */
+    public double getRate();
+}
