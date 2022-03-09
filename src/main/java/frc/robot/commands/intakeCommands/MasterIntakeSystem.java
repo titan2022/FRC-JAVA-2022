81b97e161e4d09b6e572fa73eafd33a6f36e11bc
cplepel-py <cbplepel@gmail.com>
2022-03-09 15:29:06 -0600

    Updated intake command sequencer logic

--- src/main/java/frc/robot/commands/intakeCommands/MasterIntakeSystem.java
+++ src/main/java/frc/robot/commands/intakeCommands/MasterIntakeSystem.java
@@ -1,30 +1,51 @@
 package frc.robot.commands.intakeCommands;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.IntakeSubsystem;
 
-public class MasterIntakeSystem extends ParallelRaceGroup {
+public class MasterIntakeSystem extends CommandBase {
     
-    private IntakeSubsystem sub = new IntakeSubsystem();
+    private IntakeSubsystem intake;
 
-    public MasterIntakeSystem(){
-        addCommands(
-            new SpinIntake(sub, 1.0).until(()->sub.bottomHopperBall()),
-            new SpinHopper(sub, 0.25)
-        );
+    public MasterIntakeSystem(IntakeSubsystem intake){
+        this.intake = intake;
+        addRequirements(intake);
     }
 
+    @Override
+    public void initialize() {
+        intake.raiseOrLowerIntake(Value.kForward);
+        intake.spinIntake(1.0);
+    }
+
+    @Override
+    public void execute() {
+        if(intake.topHopperBall())
+            intake.spinHopper(-0.5);
+        else if(intake.intakeBall())
+            intake.spinHopper(0.5);
+        else
+            intake.spinHopper(0.0);
+    }
+
+    @Override
     public void end(boolean interupted){
         if(interupted){
-            new SpinIntake(sub, -1.0).until(()->sub.intakeBall());
-            sub.raiseOrLowerIntake(Value.kForward);
+            intake.spinHopper(0.0);
+            new SpinIntake(intake, -1.0).until(()->!intake.intakeBall())
+                .andThen(()->intake.raiseOrLowerIntake(Value.kReverse), intake)
+                .schedule();
+        }
+        else{
+            intake.spinIntake(0.0);
+            intake.raiseOrLowerIntake(Value.kReverse);
+            new SpinHopper(intake, 1.0).until(intake::topHopperBall).schedule();
         }
     }
 
-
-
-
-
+    @Override
+    public boolean isFinished() {
+        return intake.bottomHopperBall();
+    }
 }
