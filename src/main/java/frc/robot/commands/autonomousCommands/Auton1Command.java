6dcddd069820373b6a8ab9584a0649c47dc1fc34
Ivan Chen <ivanchen3k@gmail.com>
2022-03-09 20:26:56 -0600

    Made auton 1 command

--- src/main/java/frc/robot/commands/autonomousCommands/Auton1Command.java
+++ src/main/java/frc/robot/commands/autonomousCommands/Auton1Command.java
@@ -1,23 +1,30 @@
 package frc.robot.commands.autonomousCommands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.commands.DriveToCommand;
+import frc.robot.commands.ShooterCommand;
+import frc.robot.commands.intakeCommands.MasterIntakeCommand;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.LocalizationSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 
 
-public class Auton1Command extends SequentialCommandGroup{
-    private final ShooterSubsystem shooter;
-    private final IntakeSubsystem intake;
-    private final DriveSubsystem driveBase;
+public class Auton1Command extends SequentialCommandGroup {
 
-    public Auton1Command(ShooterSubsystem shooter, IntakeSubsystem intake, DriveSubsystem driveBase) {
-        this.shooter = shooter;
-        this.intake = intake;
-        this.driveBase = driveBase;
-
+    public Auton1Command(ShooterSubsystem shooter, IntakeSubsystem intake, DriveSubsystem driveBase, LocalizationSubsystem nav) {
         addCommands(
-            
+            //Collect our ball and shoots into goal
+            new ParallelCommandGroup(new MasterIntakeCommand(intake), new DriveToCommand(driveBase, nav, 1, 1, 1)),
+            new ShooterCommand(shooter, driveBase.getRotational(), intake, nav, 0, 0, 0, 0, 50, 0.1, 0.02),
+            new ShooterCommand(shooter, driveBase.getRotational(), intake, nav, 0, 0, 0, 0, 50, 0.1, 0.02),
+            //Gets enemy team's balls
+            new ParallelCommandGroup(new MasterIntakeCommand(intake), new DriveToCommand(driveBase, nav, 1, 1, 1)),
+            new ParallelCommandGroup(new MasterIntakeCommand(intake), new DriveToCommand(driveBase, nav, 1, 1, 1)),
+            //Shoots enemy team balls away
+            new ShooterCommand(shooter, driveBase.getRotational(), intake, nav, 0, 0, 0, 0, 50, 0.1, 0.02),
+            new ShooterCommand(shooter, driveBase.getRotational(), intake, nav, 0, 0, 0, 0, 50, 0.1, 0.02),
         );
     }
 }
--- src/main/java/frc/robot/commands/autonomousCommands/Auton3Command.java
+++ src/main/java/frc/robot/commands/autonomousCommands/Auton3Command.java
@@ -16,9 +16,12 @@ public class Auton3Command extends SequentialCommandGroup {
     public Auton3Command(ShooterSubsystem shooter, IntakeSubsystem intake, DriveSubsystem driveBase, LocalizationSubsystem nav) {
         //Needs tuning
         addCommands(
+            //Collect our ball and shoots
             new ParallelCommandGroup(new MasterIntakeCommand(intake), new DriveToCommand(driveBase, nav, 1, 1, 1)),
             new ShooterCommand(shooter, driveBase.getRotational(), intake, nav, 0, 0, 0, 0, 50, 0.1, 0.02),
             new ShooterCommand(shooter, driveBase.getRotational(), intake, nav, 0, 0, 0, 0, 50, 0.1, 0.02),
+
+            //Collects two balls on the edges of field and shoot
             new ParallelCommandGroup(new MasterIntakeCommand(intake), new DriveToCommand(driveBase, nav, 1, 1, 1)),
             new ParallelCommandGroup(new MasterIntakeCommand(intake), new DriveToCommand(driveBase, nav, 1, 1, 1)),
             new ShooterCommand(shooter, driveBase.getRotational(), intake, nav, 0, 0, 0, 0, 50, 0.1, 0.02),
