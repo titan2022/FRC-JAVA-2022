ff1b5f11a506211bad45fba2a8d420151d3cdb15
cplepel-py <cbplepel@gmail.com>
2022-03-10 16:45:43 -0600

    Removed second hopper motor

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -19,18 +19,15 @@ public class IntakeSubsystem extends SubsystemBase {
 
     private static final int INTAKE_MOTOR_PORT = 15;
     private static final int HOPPER_MOTOR_PORT = 17;
-    private static final int HOPPER_MOTOR_PORT2 = 18;
     private static final int INTAKE_SENSOR_PORT = 0;
     private static final int HOPPER_SENSOR_PORT1 = 1;
     private static final int HOPPER_SENSOR_PORT2 = 2;
 
     private static final WPI_TalonFX intakeMotor = new WPI_TalonFX(INTAKE_MOTOR_PORT);
     private static final WPI_TalonFX hopperMotor = new WPI_TalonFX(HOPPER_MOTOR_PORT);
-    private static final WPI_TalonFX hopperMotor2 = new WPI_TalonFX(HOPPER_MOTOR_PORT2);
     private static final DigitalInput intakeBeamSensor = new DigitalInput(INTAKE_SENSOR_PORT);
     private static final DigitalInput bottomHopperBeamSensor = new DigitalInput(HOPPER_SENSOR_PORT1);
     private static final DigitalInput topHopperBeamSensor = new DigitalInput(HOPPER_SENSOR_PORT2);
-    //private static final Solenoid claw = new Solenoid(1);
     private static final SupplyCurrentLimitConfiguration MAX_AMPS = new SupplyCurrentLimitConfiguration(true, 10, 0, 0);
     private static final StatorCurrentLimitConfiguration MAX_AMPS_OUT = new StatorCurrentLimitConfiguration(true, 10, 0, 0);
     //Pnuematics
@@ -58,13 +55,6 @@ public class IntakeSubsystem extends SubsystemBase {
         hopperMotor.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, 0);
         hopperMotor.setInverted(true);
         hopperMotor.selectProfileSlot(0, 0);
-
-        hopperMotor2.configAllSettings(config);
-        hopperMotor2.setNeutralMode(NeutralMode.Brake);
-        hopperMotor2.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, 0);
-        hopperMotor2.setInverted(true);
-        hopperMotor2.selectProfileSlot(0, 0);
-        hopperMotor2.follow(hopperMotor);
     }
 
     public boolean intakeBall() {
