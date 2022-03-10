b7be62292498a6df44eab7db88c586f75e00b68d
Ivan Chen <ivanchen3k@gmail.com>
2022-03-09 19:05:56 -0600

    Finished the general plan for auton route 3, need to put in numbers

--- src/main/java/frc/robot/commands/autonomousCommands/Auton3Command.java
+++ src/main/java/frc/robot/commands/autonomousCommands/Auton3Command.java
@@ -1,7 +1,9 @@
 package frc.robot.commands.autonomousCommands;
 
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.commands.DriveToCommand;
+import frc.robot.commands.ShooterCommand;
 import frc.robot.commands.SpinIntake;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
@@ -9,21 +11,27 @@ import frc.robot.subsystems.LocalizationSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 
 
-public class Auton3Command1 extends ParallelCommandGroup {
+public class Auton3Command extends SequentialCommandGroup {
     private final ShooterSubsystem shooter;
     private final IntakeSubsystem intake;
     private final DriveSubsystem driveBase;
     private final LocalizationSubsystem nav;
     
-    public Auton3Command1(ShooterSubsystem shooter, IntakeSubsystem intake, DriveSubsystem driveBase, LocalizationSubsystem nav) {
+    public Auton3Command(ShooterSubsystem shooter, IntakeSubsystem intake, DriveSubsystem driveBase, LocalizationSubsystem nav) {
         this.shooter = shooter;
         this.intake = intake;
         this.driveBase = driveBase;
         this.nav = nav;
 
+        //Needs tuning
         addCommands(
-            new SpinIntake(intake, 10),
-            new DriveToCommand(driveBase, nav, 1, 1, 1)
+            new ParallelCommandGroup(new SpinIntake(intake, 10), new DriveToCommand(driveBase, nav, 1, 1, 1)),
+            new ShooterCommand(shooter, driveBase.getRotational(), intake, nav, 0, 0, 0, 0, 50, 0.1, 0.02),
+            new ShooterCommand(shooter, driveBase.getRotational(), intake, nav, 0, 0, 0, 0, 50, 0.1, 0.02),
+            new ParallelCommandGroup(new SpinIntake(intake, 10), new DriveToCommand(driveBase, nav, 1, 1, 1)),
+            new ParallelCommandGroup(new SpinIntake(intake, 10), new DriveToCommand(driveBase, nav, 1, 1, 1)),
+            new ShooterCommand(shooter, driveBase.getRotational(), intake, nav, 0, 0, 0, 0, 50, 0.1, 0.02),
+            new ShooterCommand(shooter, driveBase.getRotational(), intake, nav, 0, 0, 0, 0, 50, 0.1, 0.02)
         );
     }
 
