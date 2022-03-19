7ee1f100df7c3bd5a7137fbcbc1ff2bd64ff5b71
Anirudh Chari <anirudh.chari05@gmail.com>
2022-03-19 18:34:12 -0500

    fixed localization usage bug

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -7,51 +7,51 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.sensors.CANCoder;
 import com.ctre.phoenix.sensors.SensorInitializationStrategy;
 
-
 public class ShooterSubsystem extends SubsystemBase {
 
     private static final double MAX_VELOCITY = 10000000;
     private static final double DEFAULT_VELOCITY = 0;
     private static final int TICKS_PER_REVOLUTION = 2048;
-    private static final double HOOD_TICKS_PER_RADIAN = 2048*200;
-    private static final int HOOD_OFFSET = (int) (-2048.0*27.6/360.0);
+    private static final double HOOD_TICKS_PER_RADIAN = 2048 * 200;
+    private static final int HOOD_OFFSET = (int) (-2048.0 * 27.6 / 360.0);
     private static final int HOOD_MINIMUM_TICKS = 0;
     private static final int HOOD_MAXIMUM_TICKS = 12140;
 
     private static final int RIGHT_MOTOR_PORT = 20;
     private static final int LEFT_MOTOR_PORT = 21;
     private static final int HOOD_MOTOR_ID = 19;
-    
-    //private static final int RIGHT_ENCODER_PORT = 9;
-    //private static final int LEFT_ENCODER_PORT = 10;
-    
+
+    // private static final int RIGHT_ENCODER_PORT = 9;
+    // private static final int LEFT_ENCODER_PORT = 10;
+
     private static final WPI_TalonFX rightMotor = new WPI_TalonFX(RIGHT_MOTOR_PORT);
     private static final WPI_TalonFX leftMotor = new WPI_TalonFX(LEFT_MOTOR_PORT);
     private static final WPI_TalonFX hoodMotor = new WPI_TalonFX(HOOD_MOTOR_ID);
 
-    //private static final CANCoder rightEncoder = new CANCoder(RIGHT_ENCODER_PORT);    
-    //private static final CANCoder leftEncoder = new CANCoder(LEFT_ENCODER_PORT);   
+    // private static final CANCoder rightEncoder = new
+    // CANCoder(RIGHT_ENCODER_PORT);
+    // private static final CANCoder leftEncoder = new CANCoder(LEFT_ENCODER_PORT);
 
-    public ShooterSubsystem(){
-        //rightMotor.setInverted();
+    public ShooterSubsystem() {
+        // rightMotor.setInverted();
         rightMotor.follow(leftMotor);
         rightMotor.setSensorPhase(false);
         leftMotor.setSensorPhase(false);
         leftMotor.setInverted(false);
         rightMotor.setInverted(true);
 
-        //rightMotor.configRemoteFeedbackFilter(rightEncoder, 0);
-        //leftMotor.configRemoteFeedbackFilter(leftEncoder, 0);
-        rightMotor.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor,0,0);
-        leftMotor.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor,0,0);
-        
-        //rightEncoder.configSensorInitializationStrategy(SensorInitializationStrategy.BootToAbsolutePosition);
-        //leftEncoder.configSensorInitializationStrategy(SensorInitializationStrategy.BootToAbsolutePosition);
+        // rightMotor.configRemoteFeedbackFilter(rightEncoder, 0);
+        // leftMotor.configRemoteFeedbackFilter(leftEncoder, 0);
+        rightMotor.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, 0);
+        leftMotor.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, 0);
+
+        // rightEncoder.configSensorInitializationStrategy(SensorInitializationStrategy.BootToAbsolutePosition);
+        // leftEncoder.configSensorInitializationStrategy(SensorInitializationStrategy.BootToAbsolutePosition);
         rightMotor.configIntegratedSensorInitializationStrategy(SensorInitializationStrategy.BootToAbsolutePosition);
         leftMotor.configIntegratedSensorInitializationStrategy(SensorInitializationStrategy.BootToAbsolutePosition);
 
-        //rightEncoder.configMagnetOffset(0);
-        //leftEncoder.configMagnetOffset(0);
+        // rightEncoder.configMagnetOffset(0);
+        // leftEncoder.configMagnetOffset(0);
 
         leftMotor.config_kP(0, 100);
         leftMotor.config_kI(0, 0);
@@ -93,7 +93,7 @@ public class ShooterSubsystem extends SubsystemBase {
 
     public void setAngle(double radians) {
         double pos = radians * HOOD_TICKS_PER_RADIAN + HOOD_OFFSET;
-        if(HOOD_MINIMUM_TICKS < pos && pos < HOOD_MAXIMUM_TICKS)
+        if (HOOD_MINIMUM_TICKS < pos && pos < HOOD_MAXIMUM_TICKS)
             hoodMotor.set(ControlMode.Position, pos);
     }
 
