f483f4b673d18ca6b2abb40ae9993fe51621e231
Anirudh Chari <anirudh.chari05@gmail.com>
2022-03-19 19:22:59 -0500

    replaced beam sensor with distance sensor

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -8,9 +8,10 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.sensors.SensorInitializationStrategy;
+import com.revrobotics.Rev2mDistanceSensor;
+import com.revrobotics.Rev2mDistanceSensor.Port;
 
 import static frc.robot.Constants.Unit.*;
-
 
 public class ShooterSubsystem extends SubsystemBase {
     /** Gear ratio between the hood motor and hood rack */
@@ -25,25 +26,24 @@ public class ShooterSubsystem extends SubsystemBase {
     private static final int LEFT_MOTOR_PORT = 11;
     private static final int HOOD_MOTOR_ID = 14;
     private static final int QUEUE_MOTOR_ID = 10;
-    private static final int BEAM_BREAK_PORT = 1;
     private static final int QUEUE_SENSOR_PORT = 2;
-    
+
     private static final WPI_TalonFX rightMotor = new WPI_TalonFX(RIGHT_MOTOR_PORT);
     private static final WPI_TalonFX leftMotor = new WPI_TalonFX(LEFT_MOTOR_PORT);
     private static final WPI_TalonFX hoodMotor = new WPI_TalonFX(HOOD_MOTOR_ID);
     private static final WPI_TalonFX queueMotor = new WPI_TalonFX(QUEUE_MOTOR_ID);
-    private static final DigitalInput beamBreak = new DigitalInput(BEAM_BREAK_PORT);
+    private static final Rev2mDistanceSensor distanceSensor = new Rev2mDistanceSensor(Port.kOnboard);
     private static final DigitalInput queueSensor = new DigitalInput(QUEUE_SENSOR_PORT);
 
-    public ShooterSubsystem(){
+    public ShooterSubsystem() {
         rightMotor.follow(leftMotor);
         rightMotor.setSensorPhase(false);
         leftMotor.setSensorPhase(false);
         leftMotor.setInverted(false);
         rightMotor.setInverted(true);
 
-        rightMotor.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor,0,0);
-        leftMotor.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor,0,0);
+        rightMotor.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, 0);
+        leftMotor.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, 0);
 
         leftMotor.config_kP(0, 0.1);
         leftMotor.config_kI(0, 0);
@@ -71,17 +71,18 @@ public class ShooterSubsystem extends SubsystemBase {
     /**
      * Runs the shooter at a specified velocity.
      * 
-     * @param speed  The target velocity of the shooter in meters per second.
+     * @param speed The target velocity of the shooter in meters per second.
      */
     public void run(double speed) {
-        leftMotor.set(ControlMode.Velocity, FLYWHEEL_RATIO * speed * (M / S) / FLYWHEEL_RADIUS * RAD / (FALCON_TICKS / (100 * MS)));
+        leftMotor.set(ControlMode.Velocity,
+                FLYWHEEL_RATIO * speed * (M / S) / FLYWHEEL_RADIUS * RAD / (FALCON_TICKS / (100 * MS)));
     }
 
     /**
      * Runs the shooter at by prcent output velocity.
      * 
-     * @param percent  The target velocity of the shooter, as a proportion of the
-     *  maximum output, [-1,1].
+     * @param percent The target velocity of the shooter, as a proportion of the
+     *                maximum output, [-1,1].
      */
     public void runPercent(double percent) {
         leftMotor.set(ControlMode.PercentOutput, percent);
@@ -97,19 +98,20 @@ public class ShooterSubsystem extends SubsystemBase {
      * 
      * This method does nothing if the specified angle is out of bounds.
      * 
-     * @param radians  The angle of inclination of the hood in radians. This is
-     *  the complement of the angle of inclination of the initial velocity of a
-     *  projectile launched from the shooter.
+     * @param radians The angle of inclination of the hood in radians. This is
+     *                the complement of the angle of inclination of the initial
+     *                velocity of a
+     *                projectile launched from the shooter.
      */
     public void setAngle(double radians) {
-        if(HOOD_MIN_ANGLE < radians && radians < HOOD_MAX_ANGLE)
+        if (HOOD_MIN_ANGLE < radians && radians < HOOD_MAX_ANGLE)
             hoodMotor.set(ControlMode.Position, HOOD_RATIO * (radians * RAD - HOOD_MIN_ANGLE) / FALCON_TICKS);
     }
 
     /**
      * Runs the queue motor at a specified percent output.
      * 
-     * @param percent  The output of the queue motor, in the range [-1,1].
+     * @param percent The output of the queue motor, in the range [-1,1].
      */
     public void runQueue(double percent) {
         queueMotor.set(ControlMode.PercentOutput, percent);
@@ -118,9 +120,9 @@ public class ShooterSubsystem extends SubsystemBase {
     /**
      * Returns the current angle of the hood.
      * 
-     * @return  The current measure angle of inclination of the hood in radians.
-     *  This is the complement of the angle of inclination of the initial
-     *  velocity of a projectile launched from the shooter.
+     * @return The current measure angle of inclination of the hood in radians.
+     *         This is the complement of the angle of inclination of the initial
+     *         velocity of a projectile launched from the shooter.
      */
     public double getAngle() {
         return hoodMotor.getSelectedSensorPosition() / HOOD_RATIO * FALCON_TICKS + HOOD_MIN_ANGLE;
@@ -129,7 +131,7 @@ public class ShooterSubsystem extends SubsystemBase {
     /**
      * Returns the current velocity of the shooter.
      * 
-     * @return  The current measured velocity of the shooter in meters per second.
+     * @return The current measured velocity of the shooter in meters per second.
      */
     public double getVelocity() {
         double rawVel = (leftMotor.getSelectedSensorVelocity() + rightMotor.getSelectedSensorVelocity()) / 2;
@@ -139,8 +141,8 @@ public class ShooterSubsystem extends SubsystemBase {
     /**
      * Gets the current launch height of the shooter.
      * 
-     * @return  The current height of a projectile as it leaves the shooter, in
-     *  meters.
+     * @return The current height of a projectile as it leaves the shooter, in
+     *         meters.
      */
     public double getHeight() {
         return SHOOTER_HEIGHT;
@@ -149,17 +151,17 @@ public class ShooterSubsystem extends SubsystemBase {
     /**
      * Checks whether there is a cargo in the shooter.
      * 
-     * @return  True if a cargo is detected in the shooter, or false otherwise.
+     * @return True if a cargo is detected in the shooter, or false otherwise.
      */
     public boolean hasCargo() {
-        return beamBreak.get();
+        return distanceSensor.getRange() != -1;
     }
 
     /**
      * Checks whether there is a cargo ready to be fed into the shooter.
      * 
-     * @return  True if a cargo is detected ready to be fed into the shooter,
-     *  false otherwise.
+     * @return True if a cargo is detected ready to be fed into the shooter,
+     *         false otherwise.
      */
     public boolean hasQueue() {
         return queueSensor.get();
