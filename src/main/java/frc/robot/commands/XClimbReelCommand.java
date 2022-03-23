5438af2247eb6f73565e1ab6a2209a8239752a37
Anirudh Chari <anirudh.chari05@gmail.com>
2022-03-23 10:44:54 -0500

    removed motorized claws

--- src/main/java/frc/robot/commands/XClimbReelCommand.java
+++ src/main/java/frc/robot/commands/XClimbReelCommand.java
@@ -1,39 +1,38 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.XanderClimbSubsystem;
 
-public class XClimbReelCommand {
-    
+public class XClimbReelCommand extends CommandBase {
+
     public XanderClimbSubsystem sub;
     public double radians;
     public boolean isDone;
     public double initialPos;
-    public XClimbReelCommand(XanderClimbSubsystem sub, double radians){
+
+    public XClimbReelCommand(XanderClimbSubsystem sub, double radians) {
         this.sub = sub;
         this.radians = radians;
     }
 
+    public boolean isFinished() {
+        double netGoal = ((radians / (2 * Math.PI)) * 4096) + initialPos;
 
-    public boolean isFinished(){
-        double netGoal = ((radians/(2*Math.PI))*4096)+initialPos;
-        
-        if(netGoal>initialPos && sub.getReelTicks()>=netGoal){
+        if (netGoal > initialPos && sub.getReelTicks() >= netGoal) {
             return true;
-        }
-        else if(netGoal<initialPos && sub.getReelTicks()<=netGoal){
+        } else if (netGoal < initialPos && sub.getReelTicks() <= netGoal) {
             return true;
-        }
-        else{
+        } else {
             return false;
-        }        
+        }
     }
 
-    public void initialize(){
+    public void initialize() {
         initialPos = sub.getReelTicks();
         sub.reel(radians);
-    }    
+    }
 
-    public void execute(){
+    public void execute() {
     }
 
 }
