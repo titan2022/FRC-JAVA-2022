2c1bdda6912c4c536fa17247bc093c758f55d568
Anirudh Chari <anirudh.chari05@gmail.com>
2022-03-23 11:49:56 -0500

    Updated climb configuration

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -7,11 +7,12 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ClimbSubsystem extends SubsystemBase {
-    private static int LEFT_MOTOR_ID = 0;
-    private static int RIGHT_MOTOR_ID = 0;
+    private static int LEFT_MOTOR_ID = 9;
+    private static int RIGHT_MOTOR_ID = 17;
 
     private WPI_TalonFX leftMotor = new WPI_TalonFX(LEFT_MOTOR_ID);
     private WPI_TalonFX rightMotor = new WPI_TalonFX(RIGHT_MOTOR_ID);
@@ -26,18 +27,21 @@ public class ClimbSubsystem extends SubsystemBase {
         rightMotor.configAllSettings(config);
         leftMotor.setNeutralMode(NeutralMode.Brake);
         rightMotor.setNeutralMode(NeutralMode.Brake);
-        rightMotor.follow(rightMotor);
+        rightMotor.follow(leftMotor);
         rightMotor.setInverted(true);
     }
 
     public void runClimb(double pct) {
+        SmartDashboard.putNumber("running climb (before)", pct);
         leftMotor.set(ControlMode.PercentOutput, pct);
+        SmartDashboard.putNumber("running climb (after)", pct);
     }
 
     public void coast() {
         leftMotor.setNeutralMode(NeutralMode.Coast);
         rightMotor.setNeutralMode(NeutralMode.Coast);
     }
+
     public void brake() {
         leftMotor.setNeutralMode(NeutralMode.Brake);
         rightMotor.setNeutralMode(NeutralMode.Brake);
