c152526d9f1c3af74fd7308b7fcd4df389821ec7
Ivan Chen <ivanchen3k@gmail.com>
2022-03-25 09:56:47 -0500

    Added angle to auton 1 and 3 offest

--- src/main/java/frc/robot/commands/autonomousCommands/Auton1Command.java
+++ src/main/java/frc/robot/commands/autonomousCommands/Auton1Command.java
@@ -1,12 +1,10 @@
 package frc.robot.commands.autonomousCommands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.Constants.Unit;
-import frc.robot.commands.DriveToCommand;
-import frc.robot.commands.ShooterCommand;
-import frc.robot.commands.intakeCommands.MasterIntakeCommand;
+import frc.robot.commands.AutonRoute;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.LocalizationSubsystem;
@@ -16,18 +14,11 @@ public class Auton1Command extends SequentialCommandGroup {
 
     public Auton1Command(ShooterSubsystem shooter, IntakeSubsystem intake, DriveSubsystem driveBase,
         LocalizationSubsystem nav) {
-        nav.translateTo(new Translation2d(-23.526,-90.975));
-        // Needs to be tuned
+        nav.translateTo(new Translation2d(-23.526 * Unit.IN,-90.975 * Unit.IN));
+        nav.setOrientation(new Rotation2d(1.54461638801));
         addCommands(
-            // Collect team ball and shoots starter and team balls into goal
-            new ParallelCommandGroup(new MasterIntakeCommand(intake), new DriveToCommand(driveBase.getTranslational(), nav, -81.643 * Unit.IN, 129.396 * Unit.IN, 1)),
-            new ShooterCommand(shooter, driveBase.getRotational(), intake, nav, -2, 2, 0, 0, 1, 0.1, 0.02),
-            new ShooterCommand(shooter, driveBase.getRotational(), intake, nav, -2, 2, 0, 0, 1, 0.1, 0.02),
-
-            //Gets terminal balls team's balls
-            new ParallelCommandGroup(new MasterIntakeCommand(intake), new DriveToCommand(driveBase.getTranslational(), nav, 117.725 * Unit.IN, -282.080 * Unit.IN, 1)),
-            new ShooterCommand(shooter, driveBase.getRotational(), intake, nav, -2, 2, 0, 0, 1, 0.1, 0.02),
-            new ShooterCommand(shooter, driveBase.getRotational(), intake, nav, -2, 2, 0, 0, 1, 0.1, 0.02)
+            new AutonRoute(driveBase, intake, shooter, nav, new Translation2d(-23.526 * Unit.IN ,-90.975 -23.526 * Unit.IN), new Translation2d(-81.643 * Unit.IN, 129.396 * Unit.IN), new Translation2d(117.725 * Unit.IN, -282.080 * Unit.IN))
         );
+    
     }
 }
--- src/main/java/frc/robot/commands/autonomousCommands/Auton2Command.java
+++ src/main/java/frc/robot/commands/autonomousCommands/Auton2Command.java
@@ -5,7 +5,6 @@ import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.Constants.Unit;
 import frc.robot.commands.DriveToCommand;
 import frc.robot.commands.ShooterCommand;
-import frc.robot.commands.intakeCommands.MasterIntakeCommand;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.LocalizationSubsystem;
@@ -16,6 +15,7 @@ public class Auton2Command extends SequentialCommandGroup {
     
     public Auton2Command(ShooterSubsystem shooter, IntakeSubsystem intake, DriveSubsystem driveBase, LocalizationSubsystem nav) {
         //Needs tuning
+        /*
         addCommands(
             //Collect team ball and shoots both starter and team ball
             new ParallelCommandGroup(new MasterIntakeCommand(intake), new DriveToCommand(driveBase.getTranslational(), nav, 88.303 * Unit.IN, -121.095 * Unit.IN, 1)),
@@ -37,5 +37,6 @@ public class Auton2Command extends SequentialCommandGroup {
             //Gets to the line and prepares for player control period
             new DriveToCommand(driveBase.getTranslational(), nav, 1, 1, 1)
         );
+        */
     }
 }
